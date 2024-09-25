6c6e3d60a24627a7a97eeb061dccd53496dbeb48
vicento-cares <143.careappsoftware@gmail.com>
2024-09-25 09:34:49 +0800

    Gemba Compliance Update

--- db/ialert-query-2024-05-09.sql
+++ db/ialert-query-2024-05-09.sql
@@ -18,8 +18,8 @@ UPDATE ialert_account SET falp_group='FAP3' WHERE sections LIKE 'section6%';
 UPDATE ialert_account SET falp_group='FAP4' WHERE sections LIKE 'section5%';
 UPDATE ialert_account SET falp_group='FAP4' WHERE sections LIKE 'section7%';
 UPDATE ialert_account SET falp_group='Factory 2' WHERE sections LIKE 'section8%';
-UPDATE ialert_account SET falp_group='Production Innovation Team' WHERE sections LIKE 'repair%';
-UPDATE ialert_account SET falp_group='Production Innovation Team' WHERE sections LIKE 'PIT%';
+UPDATE ialert_account SET falp_group='Gemba Compliance' WHERE sections LIKE 'repair%';
+UPDATE ialert_account SET falp_group='Gemba Compliance' WHERE sections LIKE 'PIT%';
 UPDATE ialert_account SET falp_group='QA' WHERE sections LIKE 'qa-final%';
 UPDATE ialert_account SET falp_group='QA' WHERE sections LIKE 'qa-initial%';
 
@@ -34,8 +34,8 @@ UPDATE ialert_section SET falp_group='FAP3' WHERE section LIKE 'section6%';
 UPDATE ialert_section SET falp_group='FAP4' WHERE section LIKE 'section5%';
 UPDATE ialert_section SET falp_group='FAP4' WHERE section LIKE 'section7%';
 UPDATE ialert_section SET falp_group='Factory 2' WHERE section LIKE 'section8%';
-UPDATE ialert_section SET falp_group='Production Innovation Team' WHERE section LIKE 'repair%';
-UPDATE ialert_section SET falp_group='Production Innovation Team' WHERE section LIKE 'PIT%';
+UPDATE ialert_section SET falp_group='Gemba Compliance' WHERE section LIKE 'repair%';
+UPDATE ialert_section SET falp_group='Gemba Compliance' WHERE section LIKE 'PIT%';
 UPDATE ialert_section SET falp_group='QA' WHERE section LIKE 'qa-final%';
 UPDATE ialert_section SET falp_group='QA' WHERE section LIKE 'qa-initial%';
 UPDATE ialert_section SET falp_group='Other Group' WHERE falp_group IS NULL;
@@ -93,8 +93,8 @@ UPDATE ialert_audit SET falp_group='FAP3' WHERE section LIKE 'section6%';
 UPDATE ialert_audit SET falp_group='FAP4' WHERE section LIKE 'section5%';
 UPDATE ialert_audit SET falp_group='FAP4' WHERE section LIKE 'section7%';
 UPDATE ialert_audit SET falp_group='Factory 2' WHERE section LIKE 'section8%';
-UPDATE ialert_audit SET falp_group='Production Innovation Team' WHERE section LIKE 'repair%';
-UPDATE ialert_audit SET falp_group='Production Innovation Team' WHERE section LIKE 'PIT%';
+UPDATE ialert_audit SET falp_group='Gemba Compliance' WHERE section LIKE 'repair%';
+UPDATE ialert_audit SET falp_group='Gemba Compliance' WHERE section LIKE 'PIT%';
 UPDATE ialert_audit SET falp_group='QA' WHERE section LIKE 'qa-final%';
 UPDATE ialert_audit SET falp_group='QA' WHERE section LIKE 'qa-initial%';
 
@@ -442,8 +442,8 @@ UPDATE ialert_line_audit SET falp_group='FAP3' WHERE section LIKE 'section6%';
 UPDATE ialert_line_audit SET falp_group='FAP4' WHERE section LIKE 'section5%';
 UPDATE ialert_line_audit SET falp_group='FAP4' WHERE section LIKE 'section7%';
 UPDATE ialert_line_audit SET falp_group='Factory 2' WHERE section LIKE 'section8%';
-UPDATE ialert_line_audit SET falp_group='Production Innovation Team' WHERE section LIKE 'repair%';
-UPDATE ialert_line_audit SET falp_group='Production Innovation Team' WHERE section LIKE 'PIT%';
+UPDATE ialert_line_audit SET falp_group='Gemba Compliance' WHERE section LIKE 'repair%';
+UPDATE ialert_line_audit SET falp_group='Gemba Compliance' WHERE section LIKE 'PIT%';
 UPDATE ialert_line_audit SET falp_group='QA' WHERE section LIKE 'qa-final%';
 UPDATE ialert_line_audit SET falp_group='QA' WHERE section LIKE 'qa-initial%';
 
@@ -743,5 +743,5 @@ UPDATE ialert_line_audit SET section='sp2section9' WHERE falp_group='Secondary 2
 
 DELETE FROM ialert_section WHERE section='repair';
 
-UPDATE ialert_audit SET section='PIT' WHERE section LIKE 'repair%';
-UPDATE ialert_line_audit SET section='PIT' WHERE section LIKE 'repair%';
+UPDATE ialert_audit SET section='GC' WHERE section LIKE 'repair%';
+UPDATE ialert_line_audit SET section='GC' WHERE section LIKE 'repair%';
